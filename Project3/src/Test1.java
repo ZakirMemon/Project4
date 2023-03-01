3b0c1f887f22ab022e288889f83e3dee47784014
ZakirMemon <126395522+ZakirMemon@users.noreply.github.com>
2023-03-01 18:36:30 +0530

    Add files via upload

---
+++ Project3/src/Test1.java
@@ -0,0 +1,12 @@
+import java.io.PrintWriter;
+
+class Test1
+{
+	// Your program begins with a call to main().
+	// Prints "Hello, World" to the terminal window.
+	public static void main(String args[])
+	{
+		PrintWriter out = new PrintWriter("C:\\Users\\91832\\Documents\\Java-Projects\\Project3\\Project3\\src");
+        out.println("Hello World");
+	}
+}
